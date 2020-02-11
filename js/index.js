08bd54724ec61c7cf4243f1203dff3357ab7e357
Zahid Khawaja <zahidkhawaja@icloud.com>
2020-02-11 15:19:00 -0500

    Add mouseenter and mouseleave brightness event for header img

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
-// Your code goes here
+const busHover = document.querySelector(".intro img");
+
+busHover.addEventListener("mouseenter", () => {
+    busHover.style.filter = "brightness(95%)";
+});
+
+busHover.addEventListener("mouseleave", () => {
+    busHover.style.filter = "brightness(100%)";
+});
+
